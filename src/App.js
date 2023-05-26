072b2e1faf1c3b066c7303d733364740337a187f
pratyushkk <npratyush007@gmail.com>
2023-05-27 01:27:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incNum = () => {
+    setCount(count + 1);
+  };
+  const decNum = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <h1>{count}</h1>
+
+        <div className="btn_div">
+          <button onClick={incNum}>Increase</button>
+          <button onClick={decNum}>Decrease</button>
+        </div>
+      </div>
+    </>
   );
 }
 
